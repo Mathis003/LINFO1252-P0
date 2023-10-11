536369557b156149eb9f6417702f1ce4d82599de
Anthony <72553201+backAir@users.noreply.github.com>
2023-10-11 17:01:39 +0200

    Bugfix tout

--- memory_management.c
+++ memory_management.c
@@ -5,7 +5,10 @@ uint8_t MY_HEAP[64000];
 
 void init()
 {
-
+    for (size_t i = 0; i < 64000; i++)
+    {
+        MY_HEAP[i] = 0;
+    }
 }
 
 void *my_malloc(size_t size)
@@ -14,9 +17,13 @@ void *my_malloc(size_t size)
     while(MY_HEAP[location] & 0x1){
         location+=(MY_HEAP[location]>>1)+1;
     }
+    
+    MY_HEAP[location] = (size<<1)+0x1;
+    printf("header: %x\n",MY_HEAP[location]);
 
-    MY_HEAP[location] = (size<<1)+1;
-    return MY_HEAP+location+1;
+    // printf("location: %d\n",location);
+
+    return (void *)((char *)MY_HEAP + location + 1);
 }
 
 void my_free(void *ptr)
@@ -41,14 +48,13 @@ void print_HEAP(){
         if(!(MY_HEAP[location] & 0x1)){
             return;
         }
-        
 
         size_t size = MY_HEAP[location]>>1;
         printf("adress: %d, ", location);
         printf("value: [ ");
         for (size_t i = location+1; i < location+1+size; i++)
         {
-            printf("%x ",MY_HEAP[location+i]);
+            printf("%x ",MY_HEAP[i]);
         }
         printf("], ");
         
@@ -57,24 +63,25 @@ void print_HEAP(){
         // printf("used: %d\n", location+1);
         location+=size+1;
     }
-    
-
 }
 
 int main(int argc, char **argv)
 {
+    init();
     uint16_t *a = (uint16_t *) my_malloc(4);
     uint8_t *b = (uint8_t *) my_malloc(10);
-    *a = (uint16_t)2000;
-    *b = 43;
     my_malloc(1);
+    *b = (uint8_t)200;
+    *a = (uint16_t)2000;
+
     print_HEAP();
-    my_free(b);
-    print_HEAP();
+
+    // my_free(b);
+    // print_HEAP();
 
 
 
 
-    // printf("Test print : %d\n", 1<<5);
+    printf("Test print : %d\n", 1<<5);
     // printf("Main\n");
 }
