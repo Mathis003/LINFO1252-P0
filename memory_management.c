7c77b59f743f7e570be22ed65e1c8aade76431a4
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2023-10-11 20:40:12 +0200

    Improve print_heap() and add TODOS

--- memory_management.c
+++ memory_management.c
@@ -3,6 +3,9 @@
 
 uint8_t MY_HEAP[64000];
 
+/*
+* TODO: Faire en sorte que les blocs disponibles soit rassemblés en un gros bloc de taille 64000.
+*/
 void init()
 {
     for (size_t i = 0; i < 64000; i++)
@@ -13,13 +16,16 @@ void init()
 
 
 /*
-* TODO: Trouver un moyen de savoir si le header prend un byte ou deux bytes
+* TODO: Trouver un moyen de savoir si le header prend un byte ou deux bytes.
+* TODO: !! au types (uint8_t / uint16_t / size_t) => Pour l'instant, c'est un peu au pif.
         (tout en gardant le fait qu'on peut encoder 64000 sur 2 bytes).
 * TODO: Fragmenter les blocs de la taille voulu => Si bloc de 10 bytes dispo
         mais je veux que 4 bytes, alors je fragmente en 4 / 6 ou en 8 / 2 (dépend de l'alignement).
 * TODO: Mettre un header à la fin du bloc aussi (sera utile pour my_free()).
 * TODO: Réfléchir à la méthode pour savoir quel bloc choisir (first fit, best fit, next fit)
         => Pour l'instant, on a implémenté 'first fit'.
+* TODO: Faire en sorte que les blocs disponibles soit rassemblés en gros bloc de grande taille
+        => Pour l'instant, c'est tous des blocs individuels.
 */
 void *my_malloc(size_t size)
 {
@@ -60,40 +66,41 @@ void *my_malloc(size_t size)
 
 void my_free(void *ptr)
 {
+    // Check the validity of the argument
+    if (ptr == NULL) return;
+
+    // Get the header associated to the block allocated
     uint8_t *new_ptr = (uint8_t *) ptr;
     new_ptr--;
-    if (*new_ptr & 0x1)
-    {
-        printf("ok\n");
-        *new_ptr = *new_ptr & 0xFE;
-    }
+
+    if (new_ptr == NULL) return;
+
+    // Put the weak byte to 0 (= free block)
+    if (*new_ptr & 0x1) *new_ptr = *new_ptr & 0xFE;
 }
 
-// TODO: remove when posting to inginious
+
 void print_HEAP(){
     uint16_t location = 0;
     
-    printf("print heap:\n");
-    while (1)
+    printf("==== PRINT THE HEAP ====\n");
+    while (location <= 64000)
     {
-        // print location
-        if(!(MY_HEAP[location] & 0x1)){
-            return;
-        }
+        size_t size = MY_HEAP[location] >> 1;
 
-        size_t size = MY_HEAP[location]>>1;
         printf("adress: %d, ", location);
-        printf("value: [ ");
-        for (size_t i = location+1; i < location+1+size; i++)
-        {
-            printf("%x ",MY_HEAP[i]);
-        }
-        printf("], ");
-        
-        printf("size: %d\n", (size));
+        printf("size: %zu, ", (size));
 
-        // printf("used: %d\n", location+1);
-        location+=size+1;
+        if(MY_HEAP[location] & 0x1){
+            printf("value: [ ");
+            for (size_t i = 0; i < size; i++) printf("%x ", MY_HEAP[location + 1 + i]);
+            printf("]\n");
+        } else
+        {
+            printf("value: None (Free Block)\n");
+        }
+
+        location += size + 1;
     }
 }
 
@@ -110,10 +117,4 @@ int main(int argc, char **argv)
 
     // my_free(b);
     // print_HEAP();
-
-
-
-
-    printf("Test print : %d\n", 1<<5);
-    // printf("Main\n");
 }
