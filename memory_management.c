e1e0d952571d649b0f21b35f8c9e2a2e20859169
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2023-10-12 14:03:33 +0200

    Improve functions (but bugs for now)

--- memory_management.c
+++ memory_management.c
@@ -32,39 +32,54 @@ void init()
 */
 void *my_malloc(size_t size)
 {
-    if ((size > 64000) || (size <= 0)) return NULL;
+    uint16_t HEAP_SIZE = 64000;
 
-    uint16_t current_location = 0;
-    uint16_t available_size;
+    if ((size > HEAP_SIZE) || (size <= 0)) return NULL;
 
-    while(current_location <= 64000)
+    uint8_t current_location = 0;
+    uint8_t available_size;
+
+    while(current_location <= HEAP_SIZE)
     {
         if (MY_HEAP[current_location] & 0x1)
         {
-            current_location += (MY_HEAP[current_location] >> 1) + 1;
+            current_location += (MY_HEAP[current_location] >> 1) + 2;
         } else
         {
-            available_size = MY_HEAP[current_location] >> 1;            
+            available_size = MY_HEAP[current_location] >> 1;   
 
             if (size <= available_size)
             {
-                uint16_t remaining_size = available_size - size;
+                uint8_t remaining_size = available_size - size;
 
                 if (remaining_size > 0)
                 {
-                    MY_HEAP[current_location] = (size << 1) + 0x1;
-                    uint16_t new_header = ((remaining_size - 1) << 1);
-                    MY_HEAP[current_location + size + 1] = new_header;
+                    uint8_t headers = (size << 1) + 0x1;
+                    uint8_t new_headers = (remaining_size - size) << 1;
+
+                    // printf("size headers: %d\n", size);
+                    // printf("size new_headers: %d\n", remaining_size - size);      
+
+                    // printf("headers_begin: %d\n", current_location);
+                    // printf("headers_end: %d\n", current_location + size + 1);
+                    // printf("new_headers_begin: %d\n", current_location + size + 2);
+                    // printf("new_headers_end: %d\n", current_location + available_size + 1);
+
+                    MY_HEAP[current_location] = headers;
+                    MY_HEAP[current_location + size + 1] = headers;
+                    MY_HEAP[current_location + size + 2] = new_headers;
+                    MY_HEAP[current_location + available_size + 1] = new_headers;
                 } else
                 {
                     MY_HEAP[current_location] &= 0x1;
+                    MY_HEAP[current_location + size + 1] &= 0x1;
                 }
 
                 char *adress = ((char *) MY_HEAP + current_location + 1);
                 return (void *) adress;
             }
 
-            current_location += available_size + 1;
+            current_location += available_size + 2;
         }
     }
     return NULL;
@@ -75,24 +90,43 @@ void my_free(void *pointer)
 {
     if (pointer == NULL) return;
 
-    uint8_t *new_ptr = (uint8_t *) pointer;
-    new_ptr--;
+    uint8_t *ptr = (uint8_t *) pointer;
+    ptr--;
 
-    if ((new_ptr == NULL) | !(*new_ptr & 0x1)) return;
+    if ((ptr == NULL) | !(*ptr & 0x1)) return;
 
-    uint16_t size_block = *new_ptr >> 1;
-    uint8_t *next_header = new_ptr + size_block + 1;
+    uint8_t size_block = *ptr >> 1;
+    uint8_t *next_header = ptr + (size_block + 2);
 
-    //TODO: Same pour le header d'avant
+    uint8_t *previous_header = ptr - ((*(ptr--) >> 1) + 2);
+    ptr++;
 
-    if ((*next_header != 0) & !(*next_header & 0x1))
+    uint8_t size_total = size_block + 1;
+    uint8_t merged_header = *ptr - 0x1;
+
+    if ((next_header != NULL) & (*next_header != 0x0) & !(*next_header & 0x1))
     {
-        // Fusionner les deux blocs en un
-        uint16_t size_total = size_block + (*next_header >> 1) + 1;
-        uint8_t merged_header = size_total << 1;
-        *next_header = 0;
-        *new_ptr = merged_header;
-    } else *new_ptr -= 0x1;
+        size_total += (*next_header >> 1) + 2;
+        merged_header = size_total << 1;
+        *(next_header + (*next_header >> 1) + 1) = NULL;
+        *next_header = NULL;
+    }
+    
+    if ((previous_header != NULL) & (*previous_header != 0x0) & !(*previous_header & 0x1))
+    {
+        size_total += (*previous_header >> 1) + 2;
+        merged_header = size_total << 1;
+
+        uint8_t *tmp_ptr = ptr;
+        ptr = previous_header;
+        previous_header = tmp_ptr;
+
+        *(previous_header + (*previous_header >> 1) + 1) = NULL;
+        *previous_header = NULL;
+    }
+
+    *ptr = merged_header;
+    *(ptr + size_total + 1) = merged_header;
 }
 
 
@@ -103,7 +137,7 @@ void print_HEAP(){
     printf("==== PRINT THE HEAP ====\n");
 
     size_t size = 0;
-    while (location <= 100)
+    while (location <= 45)
     {
         // if (MY_HEAP[location] == ((uint8_t) 0)) return;
         
@@ -123,7 +157,7 @@ void print_HEAP(){
         }
         printf("]\n");
     
-        location += size + 1;
+        location += size + 2;
     }
     printf("\n");
 }
@@ -134,28 +168,29 @@ int main(int argc, char **argv)
     print_HEAP();
 
     uint16_t *a = (uint16_t *) my_malloc(4);
+    *a = (uint16_t) 2000;
+
+    print_HEAP();
+
+    my_free(a);
+
+    print_HEAP();
+
     uint8_t *b = (uint8_t *) my_malloc(10);
-    uint8_t *c = (uint8_t *) my_malloc(10);
+    uint8_t *c = (uint8_t *) my_malloc(8);
     uint8_t *d = (uint8_t *) my_malloc(11);
     *d = 122;
     *b = (uint8_t) 200;
-    *a = (uint16_t) 2000;
     *c = 12;
-
-    print_HEAP();
-
-    my_free(c);
-
-    print_HEAP();
 
     my_free(b);
 
     print_HEAP();
 
 
-    print_HEAP();
+    // print_HEAP();
 
-    uint8_t *e = (uint8_t *) my_malloc(8);
-    *e = 902;
-    print_HEAP();
+    // uint8_t *e = (uint8_t *) my_malloc(8);
+    // *e = 902;
+    // print_HEAP();
 }
