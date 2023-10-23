b27ceb37bb3194b37e5342f41a4903e755ae013a
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2023-10-23 09:27:03 +0200

    Comment code

--- memory_management.c
+++ memory_management.c
@@ -3,6 +3,8 @@
 
 const uint16_t HEAP_SIZE = 64000;
 const uint8_t SIZE_HEADER = 2;
+uint16_t nextfit_offset;
+
 uint8_t MY_HEAP[64000];
 
 
@@ -11,114 +13,148 @@ void init()
 {
     uint16_t *my_heap = (uint16_t *) MY_HEAP;
 
-    // Clear memory
-    for (uint16_t i = 0; i < HEAP_SIZE / 2; i++) my_heap[i] = 0; 
-
-    my_heap[0] = SIZE_HEADER / 2;              // nextfit ptr
-    my_heap[1] = HEAP_SIZE - 2 * SIZE_HEADER;  // size of the first free block
+    nextfit_offset = 0;                                 // nextfit offset
+    my_heap[nextfit_offset] = HEAP_SIZE - SIZE_HEADER;  // size of the first free block
 }
 
 
+/*
+* Merge the current free block with next blocks if they are availables in the heap and free.
+* @param my_heap:          pointer to the heap (uint16_t array)
+* @param loc:              location of the current free block
+* @param available_size:   pointer to the size of the current free block
+* @return:                 void
+*/
 void merge_next_free_blocks(uint16_t *my_heap, uint16_t loc, uint16_t *available_size)
 {
-    uint8_t will_merge = 0;
     uint16_t next_loc = loc + (*available_size + SIZE_HEADER) / 2;
+    uint16_t new_size_block;
 
-    while ((next_loc <= (HEAP_SIZE - 3 * SIZE_HEADER) / 2) & !(my_heap[next_loc] & 0x1))
+    while ((next_loc <= (HEAP_SIZE - 2 * SIZE_HEADER) / 2) &&  // The next block is available in the heap
+            !(my_heap[next_loc] & 0x1))                        // The next block is free
     {
-        will_merge = 1;
-        uint16_t new_size_block = my_heap[next_loc];
-        *available_size += new_size_block + SIZE_HEADER;
+        // Merge the next block with the current one
+        new_size_block = my_heap[next_loc];
         next_loc +=  (new_size_block + SIZE_HEADER) / 2;
+        *available_size += new_size_block + SIZE_HEADER;
     }
 
-    if (will_merge) my_heap[loc] = *available_size;
+    // Update the size of the current block
+    my_heap[loc] = *available_size;
 }
 
 
+/*
+* Initialize an allocated block.
+* @param my_heap:          pointer to the heap (uint16_t array)
+* @param loc:              location of the free block
+* @param available_size:   pointer to the size of the free block
+* @param size:             size requested for the allocatation
+* @return:                 1 if the block has been allocated, 0 otherwise
+*/
 uint8_t initialize_allocated_block(uint16_t *my_heap, uint16_t loc, uint16_t *available_size, uint16_t size)
 {
-    uint16_t next_block_header = loc + (size + SIZE_HEADER) / 2;
+    uint16_t next_loc = loc + (size + SIZE_HEADER) / 2;
 
-    // If the block is the last one (with no header)
-    if (next_block_header == HEAP_SIZE / 2)
+    // If the block is the last one (with no header after it) (special case)
+    if (next_loc == HEAP_SIZE / 2)
     {
-        my_heap[loc] = size + 0x1; // Mark the block as allocated
-        my_heap[0] = 1;
-        return 1;
-    } else if (next_block_header >= HEAP_SIZE / 2) return 0;
+        my_heap[loc] = size;
+        nextfit_offset = 0;        // Start from the beginning of the heap
+    }
+    // If the block is not available in the heap (special case)
+    else if (next_loc > HEAP_SIZE / 2) return 0; // Return 0 to indicate EXIT_FAILURE
 
-    if (*available_size - 2 * SIZE_HEADER >= size)
+    // If the block is not the last one and are available in the heap (general case)
+    else
     {
-        uint16_t leftovers = *available_size - size - SIZE_HEADER;
-        my_heap[next_block_header] = leftovers;
-        my_heap[loc] = size + 0x1; // Mark the block as allocated
-    } else
-    {
-        my_heap[loc] = *available_size + 0x1; // Mark the block as allocated
+        // If the block is big enough to allocate the requested size + 2 bytes for the header + remaining size
+        if (size <= *available_size - 2 * SIZE_HEADER)
+        {
+            uint16_t leftovers = *available_size - size - SIZE_HEADER;
+            my_heap[next_loc] = leftovers;
+            my_heap[loc] = size;
+        }
+        // Allocate the whole block
+        else  my_heap[loc] = *available_size;
     }
 
-    if (next_block_header >= HEAP_SIZE / 2) next_block_header = 2;
-    my_heap[0] = next_block_header;
-    return 1;
+    my_heap[loc] += 0x1; // Mark the block as allocated
+    return 1; // Return 1 to indicate EXIT_SUCCESS
 }
 
 
 void *my_malloc(size_t size, uint8_t verbose)
 {
     // Check the validity of the arguments
-    if ((size > HEAP_SIZE - 3 * SIZE_HEADER) || (size <= 0)) return NULL;
+    if ((size > HEAP_SIZE - SIZE_HEADER) || (size <= 0)) return NULL;
 
     uint16_t *my_heap = (uint16_t *) MY_HEAP;
 
-    // If uneven, add 1
-    if (size & 0x1) size++;
+    if (size & 0x1) size++; // If size is uneven, add 1
 
-    // Get the current loc in the heap
-    uint16_t initial_loc = my_heap[0];
+    // Get the initial and current loc in the heap (nextfit strategy)
+    uint16_t initial_loc = nextfit_offset;
     uint16_t loc = initial_loc;
-    uint16_t available_size;
-    uint8_t has_looped = 0;
 
-    while (((loc < HEAP_SIZE / 2) && !has_looped) || (has_looped && (loc <= initial_loc)))
+    /*
+    * If we reached the end of the heap => has_looped = 1
+    * => we can start from the beginning until initial_loc
+    * to make sure we don't miss any free block
+    */
+    uint8_t has_looped = 0;
+    uint16_t available_size;
+
+    while (((loc < HEAP_SIZE / 2) && !has_looped) ||  // If we haven't reached the end of the heap and we haven't looped
+            (has_looped && (loc <= initial_loc)))     // If we have looped and we haven't reached the initial loc
     {
-        if (my_heap[loc] & 0x1) // If the block is allocated
-        {
-            loc += (my_heap[loc] - 1 + SIZE_HEADER) / 2;
-        } else // If the block is free
+        // If the block is allocated, go to the next one
+        if (my_heap[loc] & 0x1) loc += (my_heap[loc] - 1 + SIZE_HEADER) / 2;
+
+        // If the block is free
+        else
         {
             available_size = my_heap[loc];
+
+            // Merge the next free blocks, update the available size
             merge_next_free_blocks(my_heap, loc, &available_size);
 
-            // If the block is big enough
+            // If the block is big enough to allocate the requested size
             if (size <= available_size)
             {
-                uint8_t result_value = initialize_allocated_block(my_heap, loc, &available_size, size);
-                if (result_value == 0) return NULL;
-                char *adress = ((char *) MY_HEAP + (2 * loc + SIZE_HEADER));
-                return (void *) adress;
-            }
 
-            loc += (available_size + SIZE_HEADER) / 2;
-            printf("loc: %d\n", loc);
+                // Initialize the allocated block
+                uint8_t result_value = initialize_allocated_block(my_heap, loc, &available_size, size);
+                if (result_value == 0) return NULL; // If the block is not available in the heap, return NULL
+
+                /*
+                * Get the adress of the allocated block (for a uint8_t array!)
+                * and point to the first allocated byte and return it
+                */
+                return (void *) (MY_HEAP + 2 * loc + SIZE_HEADER);
+            }
+            // If the block is not big enough, go to the next one
+            else loc += (available_size + SIZE_HEADER) / 2;
         }
 
+        // If we reached the end of the heap, start from the beginning
         if (loc >= HEAP_SIZE / 2 && has_looped == 0)
         {
             has_looped = 1;
-            loc = 1;
+            loc = 0;
         }
     }
+
+    // If we haven't found any free block, return NULL
     return NULL;
 }
-
 
 
 void my_free(void *pointer)
 {
     // Check the validity of the argument
     if (pointer == NULL) return;
-
+    
     // Get the block header
     uint16_t *header = (uint16_t *) pointer - SIZE_HEADER / 2;
 
@@ -127,24 +163,22 @@ void my_free(void *pointer)
 
     uint16_t *my_heap = (uint16_t *) MY_HEAP;
 
-    // Mark the block as free
-    *header &= 0xFFFE;
+    *header &= 0xFFFE; // Mark the block as free
 
-    // Merge the next free blocks
+    // Merge the next free blocks if they are availables in the heap and free
     merge_next_free_blocks(my_heap, header - my_heap, header);
-    
-    if ((header - my_heap < my_heap[0]) && (my_heap[0] <= my_heap[header - my_heap])) my_heap[0] = header - my_heap;
+
+    // If the nextfit offset is in the middle of the merged blocks, update it at the beginning of the merged blocks
+    uint16_t loc = header - my_heap;
+    if ((loc < nextfit_offset) && nextfit_offset <= loc + my_heap[loc]) nextfit_offset = loc; // Update the nextfit offset    
 }
-
-
-
 
 
 void print_HEAP()
 {
     printf("==== PRINT THE HEAP ====\n");
 
-    uint16_t loc = 2;
+    uint16_t loc = 0;
     uint16_t size;
 
     while (loc < HEAP_SIZE)
@@ -181,14 +215,53 @@ void print_HEAP()
 }
 
 
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     init();
+    print_HEAP();
 
-    uint8_t *first = (uint8_t *) my_malloc(63992, 0);
+    uint8_t *first = (uint8_t *) my_malloc(2, 0);
+    *first = 2;
+    print_HEAP();
+
+    uint8_t *second = (uint8_t *) my_malloc(11, 0);
+    *second = 1;
+    print_HEAP();
+
+    uint8_t *third = (uint8_t *) my_malloc(42, 0);
+    *third = 243;
+
+    uint8_t *d = my_malloc(30, 0);
+    uint8_t *e = my_malloc(30, 0);
 
     print_HEAP();
-    uint8_t *second = (uint8_t *) my_malloc(11, 0);
+
+    my_free(e);
+
+    print_HEAP();
+
+    uint8_t *f = my_malloc(45, 0);
+    print_HEAP();
+
+    uint8_t *h = my_malloc(63806, 1);
+    *f = 5;
+
+    uint8_t *dd = my_malloc(19, 0);
+    print_HEAP();
+
+    uint8_t *ssdd = my_malloc(25, 0);
+    print_HEAP();
+
+    uint8_t *ssss = my_malloc(1, 0);
+    print_HEAP();
+    
+    my_free(d);
+    my_free(third);
+    my_free(f);
+
+    print_HEAP();
+
+    uint8_t *ssssssss = my_malloc(123, 0);
+
     print_HEAP();
 }
